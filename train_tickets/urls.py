34a0a71ce6514568fc88555894c08aabd67ad503
Kawarime <kaliak.miroslav@gmail.com>
2024-02-23 19:40:23 +0200

    place_detail page

--- train_tickets/urls.py
+++ train_tickets/urls.py
@@ -3,4 +3,5 @@ from train_tickets.views import *
 
 urlpatterns = [
     path('', index, name = "index"),
+    path('detail/<int:place_id>', place_detail, name = "place_detail"),
 ]
--- train_tickets/views.py
+++ train_tickets/views.py
@@ -3,4 +3,12 @@ from train_tickets.models import *
 
 
 def index(request):
-    return render(request, "train_tickets/index.html")
+    places = Place.objects.all()
+    content = {"places":places}
+    return render(request, "train_tickets/index.html", content)
+
+
+def place_detail(request, place_id):
+    place = Place.objects.get(id=place_id)
+    content = {"place":place}
+    return render(request, "train_tickets/detail.html", content)
